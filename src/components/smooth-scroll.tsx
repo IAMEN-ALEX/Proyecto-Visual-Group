14dc3928360ec281e562f5fed1da40af35817433
Alexis Menares Lobos <menaresalexis34@gmail.com>
2026-01-12 18:50:49 -0300

    Fix: Simplify Lenis config to minimal valid options

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -7,13 +7,7 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
     useEffect(() => {
         const lenis = new Lenis({
             duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-            direction: "vertical",
-            gestureDirection: "vertical",
-            smooth: true,
-            mouseMultiplier: 1,
-            smoothTouch: false,
-            touchMultiplier: 2,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         });
 
         function raf(time: number) {
